eaca1cd6f8432566e5946f471299c19be7e61023
Amarjeet <amarjeet.btechcse@gmail.com>
2025-09-13 12:03:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    int sumOfSeries(int n) {
+        // code here
+        if(n == 0) return 0;
+        return (n*n*n) + sumOfSeries(n-1);
+    }
+}
