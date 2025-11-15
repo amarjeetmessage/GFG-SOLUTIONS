018b8270d9b0eef0d10d8472ae6368ab608d356d
Amarjeet <amarjeet.btechcse@gmail.com>
2025-11-15 13:29:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,28 +1,80 @@
-import java.util.*;
+// class Solution {
+
+//     public int knapsack(int W, int val[], int wt[]) {
+//         int n = wt.length;
+
+//         int[][] dp = new int[n + 1][W + 1];
+//         for (int i = 0; i <= n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+
+//         return solve(val, wt, W, n - 1, dp);
+//     }
+
+//     public static int solve(int[] val, int[] wt, int W, int n, int[][] dp) {
+//         // base case
+//         if (n < 0 || W == 0) return 0;
+
+//         if (dp[n][W] != -1) {
+//             return dp[n][W];
+//         }
+
+//         int take = Integer.MIN_VALUE;
+//         if (wt[n] <= W) {
+//             take = val[n] + solve(val, wt, W - wt[n], n - 1, dp);
+//         }
+
+//         int notTake = solve(val, wt, W, n - 1, dp);
+
+//         dp[n][W] = Math.max(take, notTake);
+//         return dp[n][W];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Solution {
-    static int[][] dp;
 
     public int knapsack(int W, int val[], int wt[]) {
         int n = wt.length;
-        dp = new int[n + 1][W + 1];
+
+        int[][] dp = new int[n + 1][W + 1];
+
         for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return solve(val, wt, W, n - 1);
-    }
-
-    public static int solve(int[] val, int[] wt, int W, int i) {
-        if (i < 0 || W == 0) return 0;
-
-        if (dp[i][W] != -1) return dp[i][W];
-
-        if (wt[i] <= W) {
-            int take = val[i] + solve(val, wt, W - wt[i], i - 1);
-            int notTake = solve(val, wt, W, i - 1);
-            return dp[i][W] = Math.max(take, notTake);
+            Arrays.fill(dp[i], 0);
         }
 
-        return dp[i][W] = solve(val, wt, W, i - 1);
+        for (int i = 0; i < n; i++) {
+            for (int w = 0; w <= W; w++) {
+                int take = Integer.MIN_VALUE;
+                int notTake = 0;
+
+                if (wt[i] <= w) {
+                    take = val[i] + dp[i][w - wt[i]];
+                }
+
+                notTake = dp[i][w];
+
+                dp[i + 1][w] = Math.max(take, notTake);
+            }
+        }
+
+        return dp[n][W];
     }
 }
+
+
