48f58358daa12a50eb35038b6ff7164469d77a16
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-01-28 16:48:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,6 +1,28 @@
 class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // Queue<Integer> q = new LinkedList<>();
+        // boolean vis[] = new boolean[adj.size()];
+        
+        // q.add(0);
+        // vis[0] = true;
+        
+        // while(!q.isEmpty()) {
+        //     int curr = q.peek();
+        //     q.remove();
+        //     ans.add(curr);
+            
+        //     for(int neigh : adj.get(curr)) {
+        //         if(!vis[neigh]) {
+        //             vis[neigh] = true;
+        //             q.add(neigh);
+        //         }
+        //     }
+        // }
+        
+        // return ans;
+        
         ArrayList<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         boolean vis[] = new boolean[adj.size()];
@@ -13,15 +35,14 @@ class Solution {
             q.remove();
             ans.add(curr);
             
-            for(int neigh : adj.get(curr)) {
-                if(!vis[neigh]) {
-                    vis[neigh] = true;
-                    q.add(neigh);
+            for(int ngh : adj.get(curr)) {
+                if(!vis[ngh]) {
+                    vis[ngh] = true;
+                    q.add(ngh);
                 }
             }
         }
-        
-        return ans;
+        return ans ;
     }
 }
 
