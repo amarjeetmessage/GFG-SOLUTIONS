8145de8542e105e925b61385e744a956bcc4d923
Amarjeet <177947696+amarjeetmessage@users.noreply.github.com>
2026-03-12 15:45:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,80 +1,31 @@
-// class Solution {
-
-//     public int knapsack(int W, int val[], int wt[]) {
-//         int n = wt.length;
-
-//         int[][] dp = new int[n + 1][W + 1];
-//         for (int i = 0; i <= n; i++) {
-//             Arrays.fill(dp[i], -1);
-//         }
-
-//         return solve(val, wt, W, n - 1, dp);
-//     }
-
-//     public static int solve(int[] val, int[] wt, int W, int n, int[][] dp) {
-//         // base case
-//         if (n < 0 || W == 0) return 0;
-
-//         if (dp[n][W] != -1) {
-//             return dp[n][W];
-//         }
-
-//         int take = Integer.MIN_VALUE;
-//         if (wt[n] <= W) {
-//             take = val[n] + solve(val, wt, W - wt[n], n - 1, dp);
-//         }
-
-//         int notTake = solve(val, wt, W, n - 1, dp);
-
-//         dp[n][W] = Math.max(take, notTake);
-//         return dp[n][W];
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution {
-
+    
     public int knapsack(int W, int val[], int wt[]) {
         int n = wt.length;
-
-        int[][] dp = new int[n + 1][W + 1];
-
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], 0);
+        
+        int[][] dp = new int[n+1][W+1];
+        
+        for(int i = 0; i<=n; i++) {
+            Arrays.fill(dp[i] , -1);
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int w = 0; w <= W; w++) {
-                int take = Integer.MIN_VALUE;
-                int notTake = 0;
-
-                if (wt[i] <= w) {
-                    take = val[i] + dp[i][w - wt[i]];
-                }
-
-                notTake = dp[i][w];
-
-                dp[i + 1][w] = Math.max(take, notTake);
-            }
+        return solve(wt, val, W, n,dp);
+    }
+    
+    public int solve(int wt[], int val[], int W, int n, int[][] dp) {
+        
+        if(n == 0 || W == 0) return 0;
+        
+        if(dp[n][W] != -1) {
+            return dp[n][W];
         }
-
-        return dp[n][W];
+        
+        int notpick = solve(wt, val, W, n-1,dp);
+        
+        int pick = 0;
+        if(wt[n-1] <= W) {
+            pick = val[n-1] + solve(wt, val, W - wt[n-1], n-1,dp);
+        }
+        
+        return dp[n][W] = Math.max(pick, notpick);
     }
 }
-
-
